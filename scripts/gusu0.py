ba008b01c47638252a724af1ceab4f0640372786
Yuma Ito <055matoten@gmail.com>
2022-01-11 02:51:22 +0900

    Update

--- scripts/gusu0.py
+++ scripts/gusu0.py
@@ -2,23 +2,25 @@
 
 """
 BSD 2-Clause "Simplified" License
-Copyright (c) 2022 Ryuichi Ueda.
+Copyright (c) 2022 Ryuichi Ueda & Yuma Ito.
 All rights reserved.
 """
 
 import rospy
 from std_msgs.msg import Int32
 
-n = 65
+n = 0
 
 def cb(message):
     global n
     n = message.data
     
-rospy.init_node('twice')
+rospy.init_node('gusu0')
 sub = rospy.Subscriber('count_up', Int32, cb)
-pub = rospy.Publisher('twice', Int32, queue_size=1)
+pub = rospy.Publisher('gusu0', Int32, queue_size=1)
 rate = rospy.Rate(10)
 while not rospy.is_shutdown():
+    if n % 2 == 0:
+        n = 0;
     pub.publish(n)
     rate.sleep()
